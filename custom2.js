a15b92e3cee7f1d00d1187df8e58c01bc1a0763d
heoyeji <169115914+heoyeji@users.noreply.github.com>
2024-05-08 12:29:51 +0900

    Update custom2.js

--- custom2.js
+++ custom2.js
@@ -1,16 +1,1 @@
-let user = document.querySelector("#user"),
-  play = document.querySelector("#play"),
-  result = document.querySelector("#result");
 
-function randomNum() {
-  computerNum = Math.floor(Math.random() * 100) + 1;
-  console.log(computerNum);
-}
-randomNum();
-
-play.addEventListener("click", game);
-//() 있고 없고 다름
-
-function game() {
-  console.log("게임시작");
-}
